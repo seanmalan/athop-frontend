6994e13ee27125238d44b94e0d053da731e87643
Sean Malan <104399311+seanmalan@users.noreply.github.com>
2024-04-16 17:22:26 +1200

    the navbar now has extra tabs that have dummy links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <div style={{textAlign: "center"}}>
-    <h1>
-    Seans Transport Hub
+    <>
+    <div className="mb-6">
 
-    </h1>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="mr-6"><h1>Seans Transport Hub</h1></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#features">Card</Nav.Link>
+            <Nav.Link href="#pricing">Journeys</Nav.Link>
+            <Nav.Link href="#pricing">Transactions</Nav.Link>
+            <Nav.Link href="#pricing">MyHUB</Nav.Link>
+          </Nav>
+        </Container>
+
+      </Navbar>
     </div>
-  )
-}
+    </>
+  );
+};
 
-export default Header
+export default Header;
